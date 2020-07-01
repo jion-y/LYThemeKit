aaa925df96af032ccadeab42139ee28fc9b5f396
liuming <1969489778@qq.com>
2020-07-01 23:02:01 +0800

    建立 podspec
    建立 podspec

--- LYThemeKit/LYThemeKit/LYThemeKit/LYThemeKit.h
+++ LYThemeKit/LYThemeKit/LYThemeKit/LYThemeKit.h
@@ -8,7 +8,21 @@
 
 #ifndef LYThemeKit_h
 #define LYThemeKit_h
+
 #import "LYThemeManager.h"
 #import "NSObject+LYTheme.h"
 #import "UIView+LYTheme.h"
+#import "UILabel+LYTheme.h"
+#import "UINavigationBar+LYTheme.h"
+#import "UIPageControl+LYTheme.h"
+#import "UIProgressView+LYTheme.h"
+#import "UISearchBar+LYTheme.h"
+#import "UISlider+LYTheme.h"
+#import "UISwitch+LYTheme.h"
+#import "UITabBar+LYTheme.h"
+#import "UITableView+LYTheme.h"
+#import "UITextField+LYTheme.h"
+#import "UITextView+LYTheme.h"
+#import "UIToolbar+LYTheme.h"
+
 #endif /* LYThemeKit_h */
