f1dd3dbd56d3d117f31e267e6689593cf09b9b29
liuming <1969489778@qq.com>
2020-07-02 16:44:05 +0800

    添加从配置列表读取主题的 cmd

--- LYThemeKit/LYThemeKit/LYThemeKit/LYConfigCmd.h
+++ LYThemeKit/LYThemeKit/LYThemeKit/LYConfigCmd.h
@@ -7,11 +7,13 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "LYThemeKitPotocol.h"
 NS_ASSUME_NONNULL_BEGIN
 
-@interface LYConfigCmd : NSObject
-
+@interface LYConfigCmd : NSObject<LYThemeCmdProtocol>
+@property(nonatomic,strong)NSString * themKey;
+@property(nonatomic,assign)SEL sel;
+@property(nonatomic,weak)NSObject * target;
 @end
 
 NS_ASSUME_NONNULL_END
--- LYThemeKit/LYThemeKit/LYThemeKit/LYConfigCmd.m
+++ LYThemeKit/LYThemeKit/LYThemeKit/LYConfigCmd.m
@@ -7,7 +7,56 @@
 //
 
 #import "LYConfigCmd.h"
-
+#import "LYThemeManager.h"
 @implementation LYConfigCmd
-
+-(void)execute
+{
+    NSObject * obj = [self getThemeValue];
+    NSAssert(!obj, @"not foud path for current config");
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+        if (self.target)
+        {
+            if ([self.target respondsToSelector:self.sel])
+            {
+                [self.target performSelector:self.sel withObject:obj];
+            }
+            else
+            {
+                NSString * msg = [NSString stringWithFormat:@"target not found sel %@",NSStringFromSelector(self.sel)];
+                NSAssert(NO, msg);
+            }
+        }
+    #pragma clang diagnostic pop
+}
+- (NSObject *)getThemeValue
+{
+   __block NSObject * themeObj;
+    NSDictionary * dic =  [[LYThemeManager themeManager] themeConfigDic];
+    NSArray<NSString *> * array = [self.themKey componentsSeparatedByString:@"."];
+    if (array.count > 0)
+    {
+       __block NSDictionary * preDic = dic;
+        
+        [array enumerateObjectsUsingBlock:^(NSString * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            if (idx != array.count - 1)
+            {
+                themeObj = preDic[obj];
+            }
+            else
+            {
+                NSDictionary * valueDic = preDic[obj];
+                if (valueDic) {
+                    preDic = valueDic;
+                }
+                else
+                {
+                    NSLog(@"key = %@ not foud value",obj);
+                    *stop = YES;
+                }
+            }
+        }];
+    }
+    return themeObj;
+}
 @end
