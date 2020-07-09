70d18f51bd337de78e23aad3460d02665b94f024
liuming <1969489778@qq.com>
2020-07-09 10:31:12 +0800

    test

--- LYThemeKit/LYThemeKit/ViewController.m
+++ LYThemeKit/LYThemeKit/ViewController.m
@@ -11,6 +11,8 @@
 #import "LYTestTheme.h"
 @interface ViewController ()
 @property(nonatomic,strong)LYTestTheme * theme;
+@property (weak, nonatomic) IBOutlet UIView *testview;
+
 @end
 
 @implementation ViewController
@@ -21,16 +23,19 @@
     LYTestTheme * test = [[LYTestTheme alloc] init];
     [LYThemeManager regesterWithTheme:test];
     self.theme = test;
-    
     [self.view setThemeBackGroundColor:@[[UIColor redColor],[UIColor blueColor]]];
+    
+    [self.testview setThemeBackGroundColor:@[[UIColor yellowColor], [UIColor greenColor]]];
 }
-
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     NSInteger index = self.theme.currentShowThemeIndex;
     index = (index + 1) %2;
     self.theme.currentShowThemeIndex = index;
     [self.theme switchThemes];
-    
+}
+- (void)viewWillLayoutSubviews
+{
+    [super viewWillLayoutSubviews];
 }
 @end
